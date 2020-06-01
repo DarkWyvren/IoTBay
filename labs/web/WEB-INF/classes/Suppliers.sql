2b47af8bbf245918992d371ca71a1f1bd9750943
orion-35 <study35information@gmail.com>
2020-06-01 21:10:16 +1000

    Update Suppliers.sql

--- labs/web/WEB-INF/classes/Suppliers.sql
+++ labs/web/WEB-INF/classes/Suppliers.sql
@@ -8,3 +8,17 @@
  * Created: 31/05/2020
  */
 
+CREATE TABLE SUPPLIERDB (
+    SupplierID int NOT NULL,
+    ContactName varchar(128) NOT NULL,
+    ComAddress varchar(128) NOT NULL,
+    ComNumber int,
+    ComType varchar(128),
+    PRIMARY KEY(SupplierID)
+);
+
+alter table supplierdb
+add emailaddress varchar(128);
+
+insert into supplierdb
+Values (00001, 'UNIBITZ', 'U13 Featherweight Avenue, Springfield, NSW, 2630', 0477 630 961, 'RAM', 'unibitzservice@gmail.com');
