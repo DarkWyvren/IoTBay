6264a54bdebcd0d861f3ecb7626af8cdd861f51e
orion-35 <study35information@gmail.com>
2020-06-02 11:35:22 +1000

    Create loadSuppliers.sql

---
+++ labs/build/web/DB/loadSuppliers.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  mood35-Laptop
+ */
+
+
+insert into supplierdb
+Values (00001, 'UNIBITZ', 'U13 Featherweight Avenue, Springfield, NSW, 2630', 0477 630 961, 'RAM', 'unibitzservice@gmail.com');
