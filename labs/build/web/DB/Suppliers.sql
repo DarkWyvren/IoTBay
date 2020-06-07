9a5dbb0162c48d66da0a340b2a5877a4463fea5c
orion-35 <study35information@gmail.com>
2020-06-08 00:33:34 +1000

    sql

--- labs/build/web/DB/Suppliers.sql
+++ labs/build/web/DB/Suppliers.sql
@@ -21,7 +21,8 @@ CREATE TABLE SUPPLIERDB (
 insert into supplierdb(Supname, SupAddress,SupType, SupEmail, SupStatus)
 Values 
 ('UNIBITZ', 'U13 Featherweight Avenue, Springfield, NSW, 2630', 'RAM', 'unibitzservice@gmail.com', 1),
-('Trial1', 'U17 Featherweight Avenue, Springfield, NSW, 2630', 'RAM', 'trial1@trial1.com',0);
-('Trial2', 'U18 Feaht Street, Jenkins, NSW, 2888', 'Rasberry Pi', 'trial2@trial1.com',0);
+('Trial1', 'U17 Featherweight Avenue, Springfield, NSW, 2630', 'RAM', 'trial1@trial1.com',0),
+('Trial2', 'U18 Feaht Street, Jenkins, NSW, 2888', 'Rasberry Pi', 'trial2@trial1.com',0),
+('Trial3', 'U19 Featherweight Avenue, Batovile, NSW, 2696', 'CPU', 'trial3@trial3.com',0);
 
 select * from SupplierDB;
